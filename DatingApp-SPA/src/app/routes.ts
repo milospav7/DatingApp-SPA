bbd7ad9fcdc1f693f6af64ef2ba3e33cadf7ced5
Milos Pavlovic <milos.pavlovic@dtc.rs>
2019-06-28 13:51:40 +0200

    Set angular routing

---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,20 @@
+import {Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+export const appRouts: Routes = [
+    { path: '', component: HomeComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        children: [
+            { path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
+            { path: 'messages', component: MessagesComponent},
+            { path: 'lists', component: ListsComponent}
+        ]
+    },
+    { path: '**', redirectTo: 'home', pathMatch: 'full'} // za sve ostale preusmeri ih nazad na home
+];
