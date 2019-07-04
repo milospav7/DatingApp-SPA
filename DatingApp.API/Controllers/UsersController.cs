6b28d05b6d5c89023fc74338bfe78792d4f8893b
Milos Pavlovic <milos.pavlovic@dtc.rs>
2019-07-04 15:44:57 +0200

    New DTOs

---
+++ DatingApp.API/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace DatingApp.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IDatingRepository _repo;
+        private readonly IConfiguration _config;
+
+        public UsersController(IDatingRepository repo, IConfiguration config)
+        {
+            _config = config;
+            _repo = repo;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _repo.GetUsers();
+            return Ok(users);    
+        }
+
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _repo.GetUser(id);
+            return Ok(user);    
+        }
+    }
+}
